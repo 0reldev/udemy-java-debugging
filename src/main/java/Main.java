322f0605e73f33c5d7ab989faf060a723af3ae88
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-10 11:01:23 +0200

    Introduction to debugging

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 10) {
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/main/java/StringUtilities.java
@@ -0,0 +1,10 @@
+public class StringUtilities {
+
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sBuilder, char c) {
+        this.sBuilder.append(c);
+        charsAdded++;
+    }
+}
